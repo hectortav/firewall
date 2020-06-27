afdf24ac43987e2fe535052dca9010feae36762d
ThanosKrk <thanoskara991@gmail.com>
2020-06-27 10:23:55 -0700

    Reading firewall rules from csv file

--- firewall/firewall.py
+++ firewall/firewall.py
@@ -4,27 +4,56 @@ import pox.openflow.libopenflow_01 as of
 from pox.lib.revent import *
 import pox.lib.packet as pkt
 from pox.lib.addresses import EthAddr, IPAddr
+import csv
 
 log = core.getLogger()
-rules = [['10.0.0.1','10.0.0.2'],['10.0.0.2', '10.0.0.4'], ['10.0.0.5', '10.0.0.8'], ['10.0.0.6', '10.0.0.7']]
+rulesFile = "pox/misc/firewall_rules.csv"
+
 
 class Firewall (EventMixin):
     def __init__ (self):
         log.info("Start Firewall")
         self.listenTo(core.openflow)
+        self.firewall = {}
+
+    def sendRule (self, src, dst, duration = 0):
+        if not isinstance(duration, tuple):
+            duration = (duration,duration)
+        
+        msg = of.ofp_flow_mod()
+
+        match = of.ofp_match(dl_type = 0x800, nw_proto = pkt.ipv4.ICMP_PROTOCOL)
+        match.nw_src = IPAddr(src)
+        match.nw_dst = IPAddr(dst)
+        
+        msg.match = match
+
+        self.connection.send(msg)
+
+    def addRule (self, src=0, dst=0, value=True):
+        log.info("Adding rule: source %s - destination %s", src, dst)
+        self.firewall[(src, dst)]=value
+        self.sendRule(src, dst, 10000)
         
     def _handle_ConnectionUp (self, event):
-        for rule in rules:
-            log.info("rule")
-            msg = of.ofp_flow_mod()
-            match = of.ofp_match(dl_type = 0x800, nw_proto = pkt.ipv4.ICMP_PROTOCOL)
-            match.nw_src = IPAddr(rule[0])
-            match.nw_dst = IPAddr(rule[1])
-            msg.match = match
-            msg.idle_timeout = 1000
-            msg.hard_timeout = 1000
-            msg.priority = 10
-            event.connection.send(msg)   
+
+        self.connection = event.connection
+        
+        try:
+            ifile  = open(rulesFile, "rb")
+        except Exception as e:
+            log.info(e)
+            return
+        
+        reader = csv.reader(ifile)
+        rowIndex = 0
+        for row in reader:
+            #All rows but header row
+            if rowIndex != 0:
+                self.addRule(row[1], row[2])
+            rowIndex += 1
+
+        ifile.close()
         
 def launch ():
     core.registerNew(Firewall)
--- run.sh
+++ run.sh
@@ -3,8 +3,10 @@ MININET_DIR=${PWD}
 POX_DIR=${PWD}/../../../pox/
 
 rm -rf $POX_DIR/pox/misc/firewall.py
-cp -r ./firewall/firewall.py $POX_DIR/pox/misc
+cp ./firewall/* $POX_DIR/pox/misc
+
 cd $POX_DIR
+
 #python2 pox.py pox.misc.firewall
 # ./pox.py forwarding.l2_learning openflow.discovery openflow.spanning_tree --no-flood --hold-down pox.misc.firewall
 ./pox.py log.level --DEBUG openflow.of_01 forwarding.l2_learning pox.misc.firewall
