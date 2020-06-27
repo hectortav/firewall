9688d260a5a2bea9a62a838cf8b675d9b8c73f12
hectortav <tavhector@gmail.com>
2020-06-27 21:38:09 +0300

    Combine functions

--- firewall/firewall.py
+++ firewall/firewall.py
@@ -1,57 +1,40 @@
 #!/usr/bin/python2
 from pox.core import core
-import pox.openflow.libopenflow_01 as of
+from pox.openflow.libopenflow_01 import ofp_flow_mod, ofp_match
 from pox.lib.revent import *
 import pox.lib.packet as pkt
-from pox.lib.addresses import EthAddr, IPAddr
+from pox.lib.addresses import IPAddr
 import csv
 
 log = core.getLogger()
-rulesFile = "pox/misc/firewall_rules.csv"
-
 
 class Firewall (EventMixin):
     def __init__ (self):
-        log.info("Start Firewall")
+        print("Start Firewall")
         self.listenTo(core.openflow)
         self.firewall = {}
 
-    def sendRule (self, src, dst, duration = 0):
-        if not isinstance(duration, tuple):
-            duration = (duration,duration)
-        
-        msg = of.ofp_flow_mod()
-
-        match = of.ofp_match(dl_type = 0x800, nw_proto = pkt.ipv4.ICMP_PROTOCOL)
-        match.nw_src = IPAddr(src)
-        match.nw_dst = IPAddr(dst)
-        
-        msg.match = match
-
-        self.connection.send(msg)
-
-    def addRule (self, src=0, dst=0, value=True):
-        log.info("Adding rule: source %s - destination %s", src, dst)
-        self.firewall[(src, dst)]=value
-        self.sendRule(src, dst, 10000)
-        
     def _handle_ConnectionUp (self, event):
-
         self.connection = event.connection
-        
         try:
-            ifile  = open(rulesFile, "rb")
+            ifile  = open("pox/misc/firewall_rules.csv", "rb")
         except Exception as e:
             log.info(e)
             return
         
         reader = csv.reader(ifile)
-        rowIndex = 0
         for row in reader:
-            #All rows but header row
-            if rowIndex != 0:
-                self.addRule(row[1], row[2])
-            rowIndex += 1
+            print("source: " + str(row[1]))
+            print("destination: " + str(row[2]))
+            print("~~~~~~~~~~~~~~~~")
+            self.firewall[(row[1], row[2])]=True
+            msg = ofp_flow_mod()
+            match = ofp_match(dl_type = 0x800, nw_proto = pkt.ipv4.ICMP_PROTOCOL)
+            match.nw_src = IPAddr(row[1])
+            match.nw_dst = IPAddr(row[2])
+            msg.match = match
+            msg.priority= 10
+            event.connection.send(msg)
 
         ifile.close()
         
